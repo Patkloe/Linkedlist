3ba0621c3cca0b7991ae4e4405b3c0e680212975
Patkloe <52906039+Patkloe@users.noreply.github.com>
2020-07-28 07:08:04 -0500

    Update linkedlist.js

--- linkedlist/Javascript/linkedlist.js
+++ linkedlist/Javascript/linkedlist.js
@@ -4,24 +4,32 @@ class Node{
   this.next = null;
  }
 }
-
 class Linkedlist{
  constructor(){
- this.head = null;
- this'size = 0;
+  this.head = null;
+  this.size = 0;
  }
- add(element){
-  var node = new node(element);
-  var current;
-  if(this.head === null)
-     this.head = node;
-  else{
-     current = this.head;
-     while (current.next){
+add(element){
+ var newnode = new Node(element);
+ var current;
+ if(this.head === null){
+    this.head = newnode;
+    console.log("head of the list: " + " " + this.head.element);
+ }
+ else{
+    current = this.head;
+    while(current.next){
      current = current.next;
-     }
-     current.next = node;
-  }
-  this.size++;
+    }
+     current.next = newnode;
+     console.log("new list element :" + " " + newnode.element);
  }
+    this.size++;
 }
+} // end class linkedlist
+var test = new Linkedlist();
+test.add(1);
+test.add(2);
+test.add(3);
+test.add(4);
+test.add(5);
